b37bca5c49812caf6f423919e3aaf166c782c920
SnetkovAN <monkefak@mail.ru>
2022-12-07 20:52:48 +0600

    seminar3

---
+++ C# S3 Task 19/Program.cs
@@ -0,0 +1,17 @@
+﻿// task 19
+
+Console.Clear();
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Дружище, Ваше число: {number} ПАЛИНДРОМ!!!");
+  }
+  else Console.WriteLine($"Дружище, к сожелению Ваше число: {number}  НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");

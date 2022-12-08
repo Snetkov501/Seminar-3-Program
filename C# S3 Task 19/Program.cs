9f9434df64a27b6497afb0505e1ee9040ab34431
SnetkovAN <monkefak@mail.ru>
2022-12-08 14:23:36 +0600

    seminar3 ispravlena task 19

--- C# S3 Task 19/Program.cs
+++ C# S3 Task 19/Program.cs
@@ -5,7 +5,7 @@ Console.Write("Введите число: ");
 string? number = Console.ReadLine();
 
 void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
+  if (number[0]==number[4] && number[1]==number[3]){
     Console.WriteLine($"Дружище, Ваше число: {number} ПАЛИНДРОМ!!!");
   }
   else Console.WriteLine($"Дружище, к сожелению Ваше число: {number}  НЕ палиндром.");
